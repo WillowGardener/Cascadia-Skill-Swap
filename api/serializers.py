a68d3ba90ecb5bba5fca280ed393baa5d12c17f8
Griffin Wimminger <impendingdelusion@gmail.com>
2020-09-15 13:56:49 -0700

    can access detailview from search

--- api/serializers.py
+++ api/serializers.py
@@ -64,6 +64,6 @@ class UserProfileSerializer(serializers.ModelSerializer):
     forum_posts = NestedForumPostSerializer(read_only=True, many=True)
     class Meta:        
         model = models.UserProfile
-        fields = ['id', 'username','hometown','profile_pic','experiences','teach_skills','learn_skills','forum_posts','experience_set', 'teachskill_set', 'learnskill_set']
+        fields = ['id', 'username','email','hometown','profile_pic','experiences','teach_skills','learn_skills','forum_posts','experience_set', 'teachskill_set', 'learnskill_set']
 
 
